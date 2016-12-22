e71a66e1c4a7d482773a7072b56f4d59819f6140
ukitaka <ukitaka@linecorp.com>
2016-12-22 18:41:53 +0900

    wip;

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# TODO
-SwiftOptic Library
+# SwiftOptic
 
+Optic Library for swift
+
+http://www.slideshare.net/JulienTruffaut/beyond-scala-lens
--- Tests/IsoSpec.swift
+++ Tests/IsoSpec.swift
@@ -12,12 +12,12 @@ import SwiftCheck
 
 class IsoSpec : XCTestCase {
     func testIsoLaws() {
-        property("get and reverseGet") <- forAll { (x : Int, fs : IsoOf<Int, UInt>) in
+        property("One way") <- forAll { (x : Int, fs : IsoOf<Int, UInt>) in
             let iso = Iso(get: fs.getTo, reverseGet: fs.getFrom)
             return iso.reverseGet(iso.get(x)) == x
         }
 
-        property("reverseGet and get") <- forAll { (x : UInt, fs : IsoOf<Int, UInt>) in
+        property("Other way") <- forAll { (x : UInt, fs : IsoOf<Int, UInt>) in
             let iso = Iso(get: fs.getTo, reverseGet: fs.getFrom)
             return iso.get(iso.reverseGet(x)) == x
         }
