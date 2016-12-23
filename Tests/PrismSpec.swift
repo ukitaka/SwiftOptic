63a55df72a80ca321dad3b733dcfc89506e6b586
yuki.takahashi <yuki.takahashi.1126@gmail.com>
2016-12-23 16:40:51 +0900

    wip;

---
+++ Tests/PrismSpec.swift
@@ -0,0 +1,17 @@
+//
+//  PrismSpec.swift
+//  SwiftOptic
+//
+//  Created by Yuki Takahashi on 2016/12/23.
+//  Copyright © 2016年 waft. All rights reserved.
+//
+
+import SwiftOptic
+import XCTest
+import SwiftCheck
+
+class PrismSpec : XCTestCase {
+    func testPrismLaws() {
+        //TODO
+    }
+}
