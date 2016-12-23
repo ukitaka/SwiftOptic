f84153e22af36b269cbee25002000f7f52d999e8
yuki.takahashi <yuki.takahashi.1126@gmail.com>
2016-12-23 23:19:38 +0900

    wip;

--- SwiftOptic/Lens.swift
+++ SwiftOptic/Lens.swift
@@ -8,26 +8,18 @@
 
 import Foundation
 
-struct Lens<S, A> {
-    fileprivate let _get: (S) -> A
-    fileprivate let _set: (S, A) -> S
+public struct Lens<S, A> {
+    public let get: (S) -> A
+    public let set: (A, S) -> S
 
-    init(get: @escaping (S) -> A, set: @escaping (S, A) -> S) {
-        self._get = get
-        self._set = set
+    public init(get: @escaping (S) -> A, set: @escaping (A, S) -> S) {
+        self.get = get
+        self.set = set
     }
 
-    func get(_ s: S) -> A {
-        return _get(s)
-    }
-
-    func set(_ s: S) -> (A) -> S {
-        return curry(_set)(s)
-    }
-
-    func modify(_ s: S) -> ((A) -> A) -> S {
+    public func modify(_ s: S) -> ((A) -> A) -> S {
         return { f in
-            self.set(s)(f(self.get(s)))
+            self.set(f(self.get(s)), s)
         }
     }
 }
